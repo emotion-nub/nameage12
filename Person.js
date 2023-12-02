ad8583efb3d37249f0dee17ea47e47ef97299961
Angel Turner <152616561+emotion-nub@users.noreply.github.com>
2023-12-02 15:56:09 +0100

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+}
+
+// Export the Person class
+module.exports = Person;
